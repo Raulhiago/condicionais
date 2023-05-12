d8424227ef37304e6f5982222b6f34d6bc70c084
Your Name01~ <you@example.com>
2023-05-12 15:28:22 -0300

    ex condicionais

---
+++ index.js
@@ -0,0 +1,51 @@
+//1.const pergunta = Number(prompt("qual sua idade"))
+//    if (pergunta >= 18){
+//        console.log("voce pode dirigir")
+//    }else{
+//        console.log("sinto muito")
+
+//    }
+
+
+
+//const pergunta = prompt("por favor digite M (matutino) ou V (vespertino) ou N ( Noturno).")
+//    if (pergunta === "m"){
+//        console.log("bom dia");
+//    }else if (perguta === "v"){
+//        console.log("boa tarde")
+//    }else if (pergunta === "n"){
+//        console.log("boa noite")
+//    }else {
+//        console.log("por favor digite m,v ou n")
+//    }
+
+//const turno = prompt(`informe se o seu turno é matutino (escreva a letra "M"), vespertino (Digite a letra "v") ou noturno(digite a letra "N"). `)
+//switch (turno.toLowerCase()){
+//    case "m":
+//        console.log(`Bom dia`);
+//        break
+//        case "v":
+//       console.log(`Boa tarde`);
+ //      break 
+ //      case "n":
+//        console.log(`Boa noite`)
+//        break
+//        default:
+//            console.log(`Informe seu turno pro gentileza(Utilize as letras de cada turno "M", "V" e "N"`);
+
+//}
+
+
+
+        
+    
+        
+
+ // const pergunta1 = prompt("qual genero de filme quer assistir?")      
+  //const pergunta2 = Number(prompt("qual faixa de preco?"))
+
+ // if (pergunta1=== "fantasia" && pergunta2 < 15){
+ //   console.log("bom filme")
+ // }else{
+ //   console.log("escolha outro filme: (")
+ // }
